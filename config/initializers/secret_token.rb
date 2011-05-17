b655fa4b37833acf6979fc6b981b2034ab642c6d
Aaron Karper <akarper@students.unibe.ch>
2011-05-17 21:56:02 +0200

    Ruby Ursuppe initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrsuppeRuby::Application.config.secret_token = 'a5f6f0552585188fa4df1c34628c8d51bbde968f47d7c321284bf8ce96d49a98ebfe9a1451a7413eb20cb7e0544438ee5a4f6aae2301605fa89fcaf0bc2be581'
